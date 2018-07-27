f3ff87afa45180b971b4769c6890c5616ce13ed0
David Felce <davefelce@gmail.com>
2018-07-27 20:43:31 +0100

    Uses .env file for secrets, also removed postgres dirs for sites

--- docker/postgres/estate_agent_django_demo/postgres/init1.sql
+++
@@ -1,5 +0,0 @@
-CREATE USER estateagent;
-ALTER USER estateagent WITH PASSWORD '3aPa55';
-ALTER USER estateagent CREATEDB;
-CREATE DATABASE estateagent OWNER estateagent;
-GRANT ALL PRIVILEGES ON DATABASE estateagent TO estateagent;
